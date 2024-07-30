d4cd887f218c53ea16cbf5eb7e22a7707bc17a26
NikoMyoji <nicolas.vlte@gmail.com>
2024-07-30 15:34:02 +0200

    First scene

---
+++ Assets/Scripts/GameSystem/SceneLoader.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MobileMixMashup
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        public void LoadScene (string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
