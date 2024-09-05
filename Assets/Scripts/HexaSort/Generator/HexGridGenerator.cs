8e43d41a46ec915149c4c1aacb463a90a2216d0e
NikoMyoji <nicolas.vlte@gmail.com>
2024-09-05 13:17:48 +0200

    Add flattop orientation generation and create prefab

--- Assets/Scripts/HexaSort/Generator/HexGridGenerator.cs
+++ Assets/Scripts/HexaSort/Generator/HexGridGenerator.cs
@@ -19,8 +19,6 @@ namespace MobileMixMashup.HexaSort.Generator
         [Range(0,10)]
         private int _size;
 
-        public int I;
-        public int J;
 
         // Start is called before the first frame update
         void Start()
@@ -34,28 +32,46 @@ namespace MobileMixMashup.HexaSort.Generator
         
         }
 
-
-        public void GenerateGrid ()
+        /// <summary>
+        /// Generate rectangular grid of hex cell
+        /// </summary>
+        /// <param name="size">the size of the grid, (size x size)</param>
+        public void GenerateGrid (int size, bool colorGrid = false)
         {
             float horizontalSpace = (_orientation == HexOrientation.FlatTop)?(3f / 2f) * 0.5f:(Mathf.Sqrt(3f) * 0.5f);
             float verticalSpace = (_orientation == HexOrientation.FlatTop) ? (Mathf.Sqrt(3f) * 0.5f) : (3f/2f) * 0.5f;
 
-
-            for (int i = 0; i < I; i++) //col
+            for (int i = 0; i < size; i++) //col
             {
-                for (int j=0; j < J; j++) //row
+                for (int j=0; j < size; j++) //row
                 {
-                    Vector3 pos = new Vector3(horizontalSpace * i + ((j % 2 == 0) ? 0 : horizontalSpace / 2),
-                        0,
-                        verticalSpace * j);
-
-                    GameObject GO = Instantiate(_hexPrefab, pos, Quaternion.identity);
-                    GO.GetComponent<Renderer>().material.color = Random.ColorHSV();
+                    Vector3 pos;
+                    if (_orientation == HexOrientation.PointyTop)
+                    {
+                        pos = new Vector3(  horizontalSpace * i + ((j % 2 == 0) ? 0 : horizontalSpace / 2),
+                                            0,
+                                            verticalSpace * j);
+                    }
+                    else
+                    {
+                        pos = new Vector3(  horizontalSpace * i,
+                                            0,
+                                            verticalSpace * j +((i % 2 != 0) ? 0 : verticalSpace / 2));
+                    }
+                    GameObject GO = Instantiate(_hexPrefab, pos, (_orientation == HexOrientation.FlatTop)? Quaternion.Euler(0,90f,0) : Quaternion.identity);
+                    if (colorGrid) GO.GetComponent<Renderer>().material.color = Random.ColorHSV();
                     
                 }
                 
                 
             }
+        }
+        /// <summary>
+        /// for test in the scene
+        /// </summary>
+        public void GenerateGrid ()
+        {
+            GenerateGrid(_size, true);
         }
     }
 }
