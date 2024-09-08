7af5540a9374377555697537999841bf931fe45c
NikoMyoji <nicolas.vlte@gmail.com>
2024-09-08 15:56:02 +0200

    cleanup

--- Assets/Scripts/HexaSort/Game/HexGridManager.cs
+++ Assets/Scripts/HexaSort/Game/HexGridManager.cs
@@ -1,14 +1,13 @@
 using MobileMixMashup.HexaSort.Generator;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-namespace MobileMixMashup
+namespace MobileMixMashup.HexaSort.Game
 {
     public class HexGridManager : MonoBehaviour
     {
         [SerializeField]
         private HexGridGenerator _gridGenerator;
+
         private HexaSortSettings _settings;
 
         // Start is called before the first frame update
@@ -26,19 +25,12 @@ namespace MobileMixMashup
             InitGame();
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
-
         private void InitGame()
         {
             if (_gridGenerator == null)
             {
                 Debug.LogError("generator missing");
             }
-
             _gridGenerator.GenerateGrid(_settings.FloorSize);
         }
     }
